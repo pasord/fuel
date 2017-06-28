d02516318738e51b5999558ac997eddc67a62fda
pa <1007593161@qq.com>
2017-06-28 10:26:55 +0800

    change 0628

--- js/all.js
+++ js/all.js
@@ -9,11 +9,19 @@
     "data": {
       "recommends": [
         {
-          "detailHref": "http://mp.weixin.qq.com/s/SvLoGlrW3-mgkAAXhIRz5Q",
-          "articleTitle": "对话王兴：太多人关注边界，而不关注核心",
-          "byWho": "LateNews by 小晚",
+          "date": "2017-06-28",
+          "detailHref": "http://mp.weixin.qq.com/s/bPzWQg20_Q8GMkHVEJYKLw",
+          "articleTitle": "天使投资人王怀南：宝宝树成长史",
+          "byWho": "捕手志"
         },
         {
+          "date": "",
+          "detailHref": "http://mp.weixin.qq.com/s/SvLoGlrW3-mgkAAXhIRz5Q",
+          "articleTitle": "对话王兴：太多人关注边界，而不关注核心",
+          "byWho": "LateNews by 小晚"
+        },
+        {
+          "date": "",
           "detailHref": "http://mp.weixin.qq.com/s/w8Vs7WRGmZVHLEl6-Oaa7g",
           "articleTitle": "罗辑思维的「成功」之道",
           "byWho": "MacTalk",
@@ -21,14 +29,22 @@
       ],
       "subscribes": [
         {
+          "amount": "⛽",
           "historyHref": "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MjM5MjAzODU2MA==&scene=124#wechat_redirect",
           "name": "槽边往事",
           "wxNum": "bitsea"
         },
         {
+          "amount": "⛽",
           "historyHref": "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MjM5OTgwNDQ0MQ==&scene=124#wechat_redirect",
           "name": "袁腾飞工作室",
           "wxNum": "ytfjls"
+        },
+        {
+          "amount": "1",
+          "historyHref": "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzUyMjA1MDQ0NA==&scene=124#wechat_redirect",
+          "name": "捕手志",
+          "wxNum": "ibushouzhi"
         }
       ]
     }
@@ -51,14 +67,14 @@ $recommend.click(function(event) {
   console.log(1);
   $mainUl.html(recommendHtml);
   $(this).addClass('footer-on').siblings().removeClass('footer-on');
-  $headerDesc.text('点击文字：查看文章详情');
+  $headerDesc.text('感觉文章不错就收藏吧');
 });
 // 点击订阅
 $subscribe.click(function(event) {
   console.log(2);
   $mainUl.html(subscribeHtml);
   $(this).addClass('footer-on').siblings().removeClass('footer-on');
-  $headerDesc.text('点击文字：查看历史文章');
+  $headerDesc.text('感觉好文章多就订阅吧');
 });
 
 
